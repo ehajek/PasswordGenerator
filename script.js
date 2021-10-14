7608ebd38a4ba840ed1ad52d78e45d98029dbc11
EddieH <ehajek@outlook.com>
2021-10-13 21:16:13 -0400

    first window prompt setup

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
 // Assignment code here
+var pwrdFinal = ""
+var pwrdLength
+var pwrdSpecials = false;
+var pwrdLowerCase = false;
+var pwrdUpperCase = false;
+var pwrdNumbers = false;
+const alphaLower = "abcdefghijklmnopqrstuvwxyz"
+const alphaUpper = "ABCDEFGHIGKLMNOPQRSTUVWXYZ"
+const specChar = "!#$%&\()*+,-./:;<=>?@^[\\]^_`{|}~";
 
+//fuctions
+const randNumber = Math.floor(Math.random() * 10);
+const randLowChar = alphaLower[Math.floor(Math.random() * alphaLower.length)]
+const randUppChar = alphaUpper[Math.floor(Math.random() * alphaUpper.length)]
+const randSpcChar = specChar[Math.floor(Math.random() * specChar.length)]
+
+  // window.confirm("Are you sure you'd like to quit?"); 
+
+
+
+
+//test
+console.log(randNumber);
+console.log(randLowChar);
+console.log(randUppChar);
+console.log(randSpcChar);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+generateBtn.addEventListener("click", function () {
+  pwrdLength = window.prompt("Enter the number of random password characters you would like (between 8 and 128 required).");
+  if (pwrdLength < 8 || pwrdLength > 128 || isNaN(pwrdLength)) {
+    alert("Must be numbrer between 8 and 128");
+  } 
+});
 
 // Write password to the #password input
 function writePassword() {
