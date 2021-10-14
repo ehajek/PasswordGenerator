dbc2699c0c91175dbb44c89bb6a0b1014746a7cd
EddieH <ehajek@outlook.com>
2021-10-13 21:27:58 -0400

    Created window prompts for all input variables

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Assignment code here
 var pwrdFinal = ""
 var pwrdLength
-var pwrdSpecials = false;
-var pwrdLowerCase = false;
-var pwrdUpperCase = false;
-var pwrdNumbers = false;
+var pwrdSpecials
+var pwrdLowerCase 
+var pwrdUpperCase 
+var pwrdNumbers 
 const alphaLower = "abcdefghijklmnopqrstuvwxyz"
 const alphaUpper = "ABCDEFGHIGKLMNOPQRSTUVWXYZ"
 const specChar = "!#$%&\()*+,-./:;<=>?@^[\\]^_`{|}~";
@@ -14,11 +14,7 @@ const randNumber = Math.floor(Math.random() * 10);
 const randLowChar = alphaLower[Math.floor(Math.random() * alphaLower.length)]
 const randUppChar = alphaUpper[Math.floor(Math.random() * alphaUpper.length)]
 const randSpcChar = specChar[Math.floor(Math.random() * specChar.length)]
-
-  // window.confirm("Are you sure you'd like to quit?"); 
-
-
-
+ 
 
 //test
 console.log(randNumber);
@@ -34,7 +30,17 @@ generateBtn.addEventListener("click", function () {
   if (pwrdLength < 8 || pwrdLength > 128 || isNaN(pwrdLength)) {
     alert("Must be numbrer between 8 and 128");
   } 
+  pwrdSpecials = window.confirm("Would you like to use Speical Characters?")
+  pwrdLowerCase = window.confirm("Would you like to use Lower Case Letters?")
+  pwrdUpperCase = window.confirm("Would you like to use Upper Case Letters?")
+  pwrdNumbers = window.confirm("Would you like to use Numbers?")
+ 
+
+  
+
+
 });
+
 
 // Write password to the #password input
 function writePassword() {
